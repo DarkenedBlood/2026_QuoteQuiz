05137c9aab89f8d8864d162c7ac7f9f40b633bd3
Ronan O'Brien-Hall <135774404+DarkenedBlood@users.noreply.github.com>
2026-05-26 15:08:14 +1200

    Split question and answer into seperate lists

---
+++ C_04_q_a_experiment.py
@@ -0,0 +1,16 @@
+all_quotes =  (["I'm king of the world!"],
+               ['Titanic (1997)'],
+               ['The Maltese Falcon (1941)', 'Jerry Maguire (1996)', 'Poltergeist (1982)', 'Who Framed Roger Rabbit (1988)'])
+
+question = all_quotes[0]
+right_ans = all_quotes[1]
+wrong_ans = all_quotes[2]
+
+wrong_1 = wrong_ans[0]
+wrong_2 = wrong_ans[1]
+
+print(question)
+print(right_ans)
+print(wrong_ans)
+print(wrong_1)
+print(wrong_2)
